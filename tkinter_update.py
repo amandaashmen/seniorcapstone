1fa976c32d3107520fa84f8a0984c0c63a7462bf
Amanda Ashmen <ashmena@union.edu>
2020-11-06 23:02:18 -0500

    update

---
+++ tkinter_update.py
@@ -0,0 +1,28 @@
+#LED file
+
+import tkinter as tkinter
+import tkinter.font
+from gpiozero import LED
+
+win=tk.TK()
+win.title("Using tkinter")
+myFont=tkinter.font.Font(family = 'Helvetica', size=12, weight = "bold")
+led = LED(21)
+
+def ledToggle():
+    if led.is_lit:
+        led.off()
+        ledButton["text"]="Turn LED On"
+    else:
+        led.on()
+        ledButton["text"]="Turn LED Off"
+
+def exitProgram():
+    win.quit()
+
+ledButton=tk.Button(win, text='Turn LED On', font=myFont, command=ledToggle, bg='bisque2', height=1, width=24)
+ledButton.grid(row=0, sticky=tk.NSE)
+exitButton=tk.Button(win, text='Exit', font=myFont, command=exitProgram, bg='cyan', height=1, width=6)
+exitButton.grid(row=1, sticky=tk.E)
+
+tk.mainloop()
