2a11bae2cf457e3c30bca0ee755d40ab16a95763
Amanda's Pi <ashmena@union.edu>
2021-02-04 20:07:48 -0500

    fixes

--- tk_interface.py
+++ tk_interface.py
@@ -11,15 +11,21 @@ PASSWORD = "U"
 class ARDapp(Tk):
 
     def __init__(self, *args, **kwargs):
-        
+
         Tk.__init__(self, *args, **kwargs)
         container = Frame(self)
         container.pack(side="top", fill="both", expand = True)
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-        
-        self.shared_data = {"temperature": StringVar(),
+
+        ##self.entrythingy = Entry()
+        #self.entrythingy.pack()
+
+        # Create the application variable.
+        self.contents = StringVar()
+
+        #self.shared_data = {"temperature": StringVar(),
                             "duration": StringVar()}
 
         self.frames = {}
@@ -38,7 +44,6 @@ class ARDapp(Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
-        
 class StartPage(Frame):
 
     def __init__(self, parent, controller):
@@ -53,6 +58,14 @@ class StartPage(Frame):
         button.configure(borderwidth=3)
         button.pack(pady=30)
 
+        #self.entrythingy = Entry()
+        # Set it to some value.
+        ##controller.contents.set("this is a variable")
+        # Tell the entry widget to watch this variable.
+        ##controller.entrythingy["textvariable"] = controller.contents
+        #controller.entrythingy.pack() #packs to every frame
+        ##myEntry = Entry(self, textvariable = controller.entrythingy.get())
+        ##myEntry.pack()
 
 class Modes(Frame):
 
@@ -77,12 +90,17 @@ class Modes(Frame):
         custom.configure(font='Helvetica 13')
         custom.pack(pady=15)
 
+        ##entrynew = Entry(self)
+        ##entrynew.pack()
+        ##controller.contents.set(entrynew.get())
+        # Tell the entry widget to watch this variable.
+        ##controller.entrythingy["textvariable"] = controller.contents
 
 class Locked(Frame):
 
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
-        
+
         def checkPass():
             if passEntry.get() == PASSWORD:
                 controller.show_frame(Custom)
@@ -107,20 +125,25 @@ class Custom(Frame):
         Frame.__init__(self, parent)
         label = Label(self, text="Custom Setting", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
-        
-        self.controller = controller
+
+        ##self.controller = controller
+        temp = StringVar()
 
         # Once the Submit button is pressed, this function will execute
         def set_variables():
-            global TEMP_VAR
-            TEMP_VAR = tempEntry.get()
-            print("custom")
-            print(TEMP_VAR)
-            print("above")
-            global DUR_VAR
-            DUR_VAR = timeEntry.get()
+            #global TEMP_VAR
+            #TEMP_VAR = tempEntry.get()
+            #print("custom")
+            #print(TEMP_VAR)
+            #print("above")
+            #global DUR_VAR
+            #DUR_VAR = timeEntry.get()
             #controller.show_frame(Confirm)
-            controller.frames[Confirm].printHi
+            #con = Confirm(parent, controller)
+            #con.printHi()
+            #controller.frames[Confirm].printHi
+            controller.contents.set(temp.get())
+            controller.shared_data["temperature"] = temp.get()
             controller.show_frame(Confirm)
 
         temp_set = Label(self, text="Set Temperature (F)", font=SMALL_FONT)
@@ -129,7 +152,7 @@ class Custom(Frame):
         time_set = Label(self, text="Set Duration (min.)", font=SMALL_FONT)
         time_set.place(x=70,y=130)
 
-        tempEntry= Entry(self, width=3, font=("Arial",18,""))
+        tempEntry= Entry(self, width=3, font=("Arial",18,""), textvariable=temp)
         tempEntry.place(x=260,y=90)
 
         timeEntry= Entry(self, width=3, font=("Arial",18,""))
@@ -143,16 +166,17 @@ class Custom(Frame):
 
 
 class Confirm(Frame):
-
+    #def printHi(self):
+        #print('hi')
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
-        self.controller = controller # necessda?
-        
+        #self.controller = controller # necessda?
+
         # Declaration of variables
         #minute=DUR_VAR
         minute=DUR_VAR
         second=StringVar()
-  
+
         # setting the default value as 0
         #minute.set("9"+DUR_VAR+"9")                    #DUR_VAR makes it blank
         second.set("00")
@@ -161,12 +185,22 @@ class Confirm(Frame):
         #while(1):
         current_temp = Label(self, text="Current Temperature: 70", font=SMALL_FONT)
         current_temp.place(x=100,y=110)
-            
+
         desired_temp = Label(self, text="Desired temperature: ", font=SMALL_FONT)
         desired_temp.place(x= 100, y = 140)
-        tempLabel = Label(self, text=TEMP_VAR, font=SMALL_FONT)
-        #tempLabel.place(x= 170, y = 140)
-            
+
+        #tempLabel = Label(self, font=SMALL_FONT, textvariable= controller.shared_data["temperature"])
+        tempLabel = Label(self, font=SMALL_FONT, textvariable= controller.contents)
+        tempLabel.place(x= 170, y = 140)
+
+        #contents = StringVar()
+        # Set it to some value.
+        #contents.set(TEMP_VAR)
+        #maqybe make contents global and set in the other frame
+        # Tell the entry widget to watch this variable.
+        #tempLabel["textvariable"] = contents
+
+
         timeLabel = Label(self, text="Timer: "+DUR_VAR+":", font=SMALL_FONT)
         secondLabel =  Label(self, textvariable=second, font=SMALL_FONT)
         secondLabel.place(x= 195, y = 170)
@@ -174,30 +208,26 @@ class Confirm(Frame):
 
         def update():
                 #global TEMP_VAR
-            
+
             print(TEMP_VAR)
             tempLabel.config(text=TEMP_VAR)
-            
+
         #self.parent.after(100, update)
         self.after(100, update)
         #self.controller.after(100, update)
 
-        def printHi():
+        def printHi(self):
             print("confirm")
-            print(TEMP_VAR)
-            tempLabel.config(text=TEMP_VAR)
-            print('config')
-            tempLabel.place(x= 170, y = 140)
 
         #self.printHi = printHi()
 
             #self.update()
             #time.sleep(1)
 
-            # Use of Entry class to take input from the user  
+            # Use of Entry class to take input from the user
         #minuteEntry= Entry(self, width=3, font=("Arial",18,""), textvariable=minute)
         #minuteEntry.place(x=170,y=170)
-  
+
             #secondEntry= Entry(self, width=3, font=("Arial",18,""), textvariable=second)
         #secondEntry.place(x=217,y=170)
 
@@ -210,7 +240,7 @@ class Confirm(Frame):
 
             button['text'] = 'End'
             button['command'] = lambda: controller.show_frame(EndPage)
-            #button['command'] = lambda: reformat   
+            #button['command'] = lambda: reformat
 
             #try:
                 # the input provided by the user is
@@ -222,47 +252,58 @@ class Confirm(Frame):
                 #controller.show_frame(EndPage)
 
                 # divmod(firstvalue = temp//60, secondvalue = temp%60)
-                mins,secs = divmod(temp, 60) 
-           
-                # using format () method to store the value up to 
+                mins,secs = divmod(temp, 60)
+
+                # using format () method to store the value up to
                 # two decimal places
                 #secondLabel.config(text="{0:2d}".format(secs))
                 #minute.set("{0:2d}".format(mins))
                 #second.set("{0:2d}".format(secs))
-  
+
                 # updating the GUI window after decrementing the
                 # temp value every time
                 #self.update()
                 #time.sleep(1)
-  
+
                 # when temp value = 0; then a messagebox pop's up
                 # with a message:"Time's up"
                 if (temp == 0):
                     messagebox.showinfo("Time Countdown", "Time's up ")
-         
+
                 # after every one sec the value of temp will be decremented
                 # by one
                 temp -= 1
 
 
- 
+
         title = Label(self, text="Confirm", font=LARGE_FONT)
         title.pack(pady=10,padx=10)
 
         #showTemp = Button(self, text="Click to check mode", command = printMode, fg="navy", font="Helvetica 14")
         #showTemp.pack(pady=10)
-        
+
         back = Button(self, text="Back to Modes", command=lambda: controller.show_frame(Modes))
         back.place(x=70, y=250)
 
         begin = Button(self, text="Begin", command=lambda: submit(begin))
         begin.place(x=250, y=250)
 
+    def printHi(self):
+        tempLabel = Label(self, text=TEMP_VAR, font=SMALL_FONT)
+        tempLabel.pack()
+        print("confirm")
+        print(TEMP_VAR)
+        tempLabel.config(text=TEMP_VAR)
+        print('config')
+        tempLabel.place(x= 170, y = 140)
+        begin = Button(self, text="Begdfgin", command=lambda: submit(begin))
+        begin.place(x=300, y=250)
+
 class EndPage(Frame):
 
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
-        
+
         label = Label(self, text="Treatment has ended", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
@@ -271,7 +312,7 @@ class EndPage(Frame):
 
         done = Button(self, text="Return to Home", command=lambda: controller.show_frame(StartPage))
         done.pack()
-        
+
 app = ARDapp()
 app.title("ARD Interface")
 app.mainloop()
