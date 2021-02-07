92b559ca5fa19cb4f031ea3206bb961d98c4402c
amandaashmen <56096348+amandaashmen@users.noreply.github.com>
2021-02-07 15:49:56 -0500

    Update pi_output.py

--- pi_output.py
+++ pi_output.py
@@ -3,6 +3,8 @@ import board
 import busio
 import adafruit_mcp4725
 
+voltage = 2.5   # desired voltage value
+
 # to use physical board pin numbers
 GPIO.setmode(GPIO.BCM)
 
@@ -13,39 +15,29 @@ GPIO.setup(channel, GPIO.OUT)
 # set the output state of a GPIO pin
 GPIO.output(channel, GPIO.HIGH)
 
-# done with library, free up resources & return back to default
-#gitGPIO.cleanup()
-
-# SPDX-FileCopyrightText: 2018 Tony DiCola for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# Simple demo of setting the DAC value up and down through its entire range
-# of values.
-
 # Initialize I2C bus.
 i2c = busio.I2C(3, 2)
 
 # Initialize MCP4725.
 dac = adafruit_mcp4725.MCP4725(i2c, address=0x60)
-# Optionally you can specify a different addres if you override the A0 pin.
-#amp = adafruit_max9744.MAX9744(i2c, address=0x63)
 
-# There are a three ways to set the DAC output, you can use any of these:
-dac.value = 65535  # Use the value property with a 16-bit number just like
+# There are a three ways to set the DAC output:
+#dac.value = 65535  # Use the value property with a 16-bit number just like
 # the AnalogOut class.  Note the MCP4725 is only a 12-bit
 # DAC so quantization errors will occur.  The range of
 # values is 0 (minimum/ground) to 65535 (maximum/Vout).
 
-dac.raw_value = 4095  # Use the raw_value property to directly read and write
+#dac.raw_value = 4095  # Use the raw_value property to directly read and write
 # the 12-bit DAC value.  The range of values is
 # 0 (minimum/ground) to 4095 (maximum/Vout).
 
-dac.normalized_value = 1.0  # Use the normalized_value property to set the
+#dac.normalized_value = 1.0  # Use the normalized_value property to set the
 # output with a floating point value in the range
 # 0 to 1.0 where 0 is minimum/ground and 1.0 is
 # maximum/Vout.
 
 # Main loop will go up and down through the range of DAC values forever.
+# while True: #use this line for cycling between a range of values
 while False:
     # Go up the 12-bit raw range.
     print("Going up 0-3.3V...")
@@ -56,6 +48,7 @@ while False:
     for i in range(4095, -1, -1):
         dac.raw_value = i
 
-dac.normalized_value = 1
+dac.normalized_value = voltage/3.55
 
-
+# done with library, free up resources & return back to default
+#gitGPIO.cleanup()
