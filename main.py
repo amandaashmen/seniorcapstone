2eac50e8f052aab29f3ba7a0400c6fa0e152dc17
amandaashmen <56096348+amandaashmen@users.noreply.github.com>
2021-02-25 16:10:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,8 @@ app.mainloop()
 app.controller.temperature 
 control.setTarget(60)
 
-## jk import control in the interface
+## import control in the interface
+## set the target temp using the setTemp() function in controller
+## fed that in ith the controller.temp attribute at the confirm frame
+## next point of communication is getting the current temp from the thermistor - should I represent as individual therm or avg
+## should i make a getter function for the avg therm temp
