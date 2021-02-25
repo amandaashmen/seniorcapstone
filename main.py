8a75e9a3d8514087abbee27d4070318495d12bf6
amandaashmen <56096348+amandaashmen@users.noreply.github.com>
2021-02-25 15:53:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import tk_interface
 import control
 
+
+app = ARDapp()
+app.title("ARD Interface")
+app.mainloop()
+
+
+
+app.controller.temperature 
 control.setTarget(60)
+
+## jk import control in the interface
