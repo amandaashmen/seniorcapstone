6bda083719758da1e9a0228af2603a64abe3293c
amandaashmen <56096348+amandaashmen@users.noreply.github.com>
2021-03-01 14:18:07 -0500

    Update tk_interface.py

--- tk_interface.py
+++ tk_interface.py
@@ -160,7 +160,7 @@ class Confirm(Frame):
         current_temp = Label(self, text="Current Temperature: 70", fg="navy", font=SMALL_FONT)
         current_temp.place(x=100,y=110)
         # FIX instead of 70, make it the avg thermistor readings
-        therm_temp = Label(self, font=SMALL_FONT, textvariable= control.getAverage) # ()? need to update constantly
+        therm_temp = Label(self, font=SMALL_FONT, textvariable= controller.getAverageTemp) # ()? need to update constantly
         therm_temp.place(x= 140, y = 110)
 
         desired_temp = Label(self, text="Desired temperature: ", fg="navy", font=SMALL_FONT)
