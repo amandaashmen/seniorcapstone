bd60a48a87eb41245ed4ef3c16d1e2bed81e9ff7
amandaashmen <56096348+amandaashmen@users.noreply.github.com>
2021-03-07 18:45:33 -0500

    Update control.py

--- control.py
+++ control.py
@@ -217,7 +217,7 @@ def updatePID(current_temp, pelt, dac_no, therm):
     """
     pelt.update(current_temp)                                    # update pid system with current thermistor temperature
     target_out_temp = pelt.output
-    convert_T_to_V(target_out_temp)
+    print(convert_T_to_V(target_out_temp))
     dac_out = max(min(convert_T_to_V(target_out_temp), MAX_PELT), 0) # scales output to maximum voltage peltier can handle
     dac_no.normalized_value = dac_out/MAX_DAC                           # set pin output to desired voltage value
     #dac_no.normalized_value = 0.0
