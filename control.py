9e9ad593f4935bce5d0514994a319a1ed09f2709
amandaashmen <56096348+amandaashmen@users.noreply.github.com>
2021-03-07 19:23:19 -0500

    Update control.py

--- control.py
+++ control.py
@@ -50,7 +50,7 @@ chan1 = AnalogIn(mcp, MCP.P1)                                        # create an
 ADC_MAX = 65535
 
 ## PID SET-UP
-Kp = 55.0                               # proportional gain
+Kp = 1000.0                               # proportional gain
 Ki =  0.0                               # integral gain
 Kd =  0.0                               # derivative gain
 TARGET = 50                             # degrees
